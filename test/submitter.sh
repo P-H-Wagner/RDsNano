825ce03df1a63028b1095dc162ba41ac6a83f257
Patricia Wagner <115067751+P-H-Wagner@users.noreply.github.com>
2023-12-18 10:39:20 +0100

    commit cfg's and submitter

---
+++ test/submitter.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /work/pahwagne/CMSSW_10_6_37/src/PhysicsTools/RDsNano/test
+scramv1 runtime -sh
+mkdir -p /scratch/pahwagne/nanoAOD
+ls /scratch/pahwagne/
+cmsRun run_nano_jpsi_cfg.py
+xrdcp /scratch/pahwagne/nanoAOD/test.root root://t3dcachedb.psi.ch:1094///pnfs/psi.ch/cms/trivcat/store/user/pahwagne/nanoAOD/test.root
+rm /scratch/pahwagne/nanoAOD/test.root

6ba657e62f852a092df30abd199a044a92a65ed5
Patricia Wagner <115067751+P-H-Wagner@users.noreply.github.com>
2023-12-18 10:42:24 +0100

    rjpsi -> rds

--- test/submitter.sh
+++ test/submitter.sh
@@ -3,6 +3,6 @@ cd /work/pahwagne/CMSSW_10_6_37/src/PhysicsTools/RDsNano/test
 scramv1 runtime -sh
 mkdir -p /scratch/pahwagne/nanoAOD
 ls /scratch/pahwagne/
-cmsRun run_nano_jpsi_cfg.py
+cmsRun run_nano_rds_cfg.py
 xrdcp /scratch/pahwagne/nanoAOD/test.root root://t3dcachedb.psi.ch:1094///pnfs/psi.ch/cms/trivcat/store/user/pahwagne/nanoAOD/test.root
 rm /scratch/pahwagne/nanoAOD/test.root
